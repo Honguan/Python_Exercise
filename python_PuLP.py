a8771821ba031e87b4045536a76a5a4c1b35a013
Honguan <666ddxx@gmail.com>
2023-03-11 15:56:47 +0800

    add python_PuLP

---
+++ python_PuLP.py
@@ -0,0 +1,24 @@
+from pulp import *
+
+# 建立問題
+prob = LpProblem("Maximize Revenue", LpMaximize)
+
+# 定義變量
+x = LpVariable("x", lowBound=0)
+y = LpVariable("y", lowBound=0)
+
+# 定義目標函數
+prob += 18*x + 16*y, "Total Revenue"
+
+# 定義約束條件
+prob += 6*x + 5*y <= 8
+prob += 6*x + 3.5*y <= 7
+prob += 2.5*x + 4*y <= 5
+
+# 求解問題
+prob.solve()
+
+# 印出結果
+print("最大收益 = ", value(prob.objective))
+print("x = ", value(x))
+print("y = ", value(y))
