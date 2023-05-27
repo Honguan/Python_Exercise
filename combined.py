4e53a7cf609d6cca9c365de2f6e499aaa0e8b6df
Honguan <666ddxx@gmail.com>
2023-05-27 15:23:11 +0800

    修正錯誤

--- combined.py
+++ combined.py
@@ -1,19 +1,19 @@
 import os
 
-# 指定目标文件夹路径
-folder_path = r"C:\Users\ChenPi\Documents\GitHub\nanoGPT\data\shakespeare_char\testdate"
+# 指定目標文件夾路徑
+folder_path = input(r"請輸入路徑: ")
 
-# 获取目标文件夹中所有 .txt 文件的路径列表
+# 獲取目標文件夾中所有 .txt 文件的路徑列表
 txt_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.endswith(".txt")]
 
-# 创建一个新的文件来保存汇总内容
+# 創建一個新的文件來保存匯總內容
 output_file = open("combined.txt", "w", encoding="utf-8")
 
-# 遍历所有 .txt 文件并将其内容追加到汇总文件中
+# 遍歷所有 .txt 文件並將其內容追加到匯總文件中
 for file_path in txt_files:
     with open(file_path, "r", encoding="utf-8") as file:
         content = file.read()
         output_file.write(content + "\n")
 
-# 关闭文件
+# 關閉文件
 output_file.close()
--- indexset.py
+++ indexset.py
@@ -1,22 +1,22 @@
 import os
 
-# 指定目标文件夹路径
-folder_path = r"C:\Users\ChenPi\Documents\GitHub\nanoGPT\data\shakespeare_char\testdate"
+# 指定目標文件夾路徑
+folder_path = input(r"請輸入路徑: ")
 
-# 获取目标文件夹中所有文件的路径列表
+# 獲取目標文件夾中所有文件的路徑列表
 files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-# 按文件名进行排序
+# 按文件名進行排序
 files.sort()
 
-# 遍历所有文件并逐个重新命名
+# 遍歷所有文件並逐個重新命名
 for index, file_name in enumerate(files):
     file_path = os.path.join(folder_path, file_name)
     file_extension = os.path.splitext(file_name)[1]
     new_file_name = f"{index+1}{file_extension}"
     new_file_path = os.path.join(folder_path, new_file_name)
     
-    # 检查目标文件是否存在，如果存在则添加适当的编号
+    # 檢查目標文件是否存在，如果存在則添加適當的編號
     counter = 1
     while os.path.exists(new_file_path):
         new_file_name = f"{index+1}_{counter}{file_extension}"
